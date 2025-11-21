0da7440a8ccf417a2ffc15ff4fb5578ca4d7300c
sivalaxmi19 <sivalaxmikosana2006@gmail.com>
2025-11-21 12:15:05 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Save this file as HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, World!");
+    }
+}
+
